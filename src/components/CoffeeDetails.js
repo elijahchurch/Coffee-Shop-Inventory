db48399b52ea810394db836bd008fbd4844d7035
Jason E. Church <elijahchurch@gmail.com>
2023-09-08 16:01:38 -0700

    Add branching logic to CoffeeDetails to display message if inventory is 0

--- src/components/CoffeeDetails.js
+++ src/components/CoffeeDetails.js
@@ -3,6 +3,13 @@ import PropTypes from "prop-types";
 
 function CoffeeDetails(props){
     const {coffee, onClickingDelete, onClickingEdit} = props;
+    let inventory = null;
+    if(coffee.inventory === 0) {
+        inventory = "Out of Stock!";
+    } else {
+        inventory = `${coffee.inventory} lbs.`
+    }
+    
     return(
     <div className="container main">
         <h2>{coffee.name} Details</h2>
@@ -10,7 +17,7 @@ function CoffeeDetails(props){
         <p>Exported from: {coffee.origin}</p>
         <p>Roast: {coffee.roast}</p>
         <p>Price: ${coffee.cost}</p>
-        <p>Amount of Inventory Left: {coffee.inventory}lbs.</p>
+        <p>Inventory: {inventory}</p>
         <hr/>
         <button onClick = {onClickingEdit}>Update</button>
         <button onClick = {()=> onClickingDelete(coffee.id)}>Remove from Menu</button>
